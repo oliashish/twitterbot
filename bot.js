8ffc2bab8cbc8b413ad28f51b651089c739d9dc2
Ashish oli <oliashish3@gmail.com>
2020-10-23 21:49:14 +0530

    This is a twitter bot

---
+++ bot.js
@@ -0,0 +1,36 @@
+const config = require("./config");
+const twit = require("twit");
+const T = new twit(config);
+
+function retweet() {
+     let params = {
+          q: `#javascript`,
+     };
+     T.get("search/tweets", params, (err, data, res) => {
+          let tweets = data.statuses;
+          if (!err) {
+               for (let dat of tweets) {
+                    let retweetID = dat.id_str;
+                    let tweet = dat.text
+                    T.post(
+                         "statuses/retweet/:id",
+                         { id: retweetID },
+                         (err, res) => {
+                              if (res) {
+                                   console.log("Retweeted!!" + retweetID);
+                                   console.log(tweet);
+                              }
+                              if (err) {
+                                   console.log(
+                                        "something went wronge while aborting..." +
+                                             err
+                                   );
+                              }
+                         }
+                    );
+               }
+          }
+     });
+}
+
+setInterval(retweet, 20000);
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+     consumer_key: `3LcYYHhos3OYbB7iPIrGDMrR9`,
+     consumer_secret: `xYCfZPkM4yD2UmKsuxeEv5URjwQFr0xNbl75AP5KafYxcQ5QW8`,
+     access_token: `1250638109722599430-dhqI1KRZhMIpdpUAa1sttEGtFMKwQ1`,
+     access_token_secret: `QpoA1gHGx0vCC4RyFOeoBWW92J7XNKiJhPuxZ98hZX3xU`,
+};
